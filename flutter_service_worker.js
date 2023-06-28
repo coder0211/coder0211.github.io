18b9e244049806158bbe957b1bc62920603bb593
coder0211 <hoand1@hasaki.vn>
2023-06-28 14:46:14 +0700

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "c6c250e0039bd2343ab2adb49adb5d2a",
-"index.html": "2a2326ae3bc5f233147236b68fb2feab",
-"/": "2a2326ae3bc5f233147236b68fb2feab",
-"main.dart.js": "2fa1cc9b65e8afe7e3502f3ac5659c9c",
+"index.html": "3ee594b5192d2bd044a50e56bbe2ccb7",
+"/": "3ee594b5192d2bd044a50e56bbe2ccb7",
+"main.dart.js": "1f3fb8c5824cb6510c99e7c201653cca",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "ce132898dcf9f43264f463922ee62257",
 "icons/Icon-192.png": "ce132898dcf9f43264f463922ee62257",
